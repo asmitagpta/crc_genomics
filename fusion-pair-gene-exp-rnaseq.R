58d9f75e6c5354481fdc99a062f497572ebe245c
asmi.g <gupta.asmita86@gmail.com>
2024-06-03 13:59:01 +0530

    Update fusion-pair-gene-exp-rnaseq.R

--- fusion-pair-gene-exp-rnaseq.R
+++ fusion-pair-gene-exp-rnaseq.R
@@ -42,30 +42,17 @@ exp_counts_sub_data <- gather(exp_counts_sub, key = 'sample_ID', value = 'counts
 colnames(exp_counts_sub_data) <- c('gene_name','pair','gene_loc','sample_ID','counts')
 exp_counts_sub_data <- left_join(exp_counts_sub_data, cdfd_id, by='sample_ID')
 
-## sub data for rapsac meeting
-#exp_counts.s <- exp_counts %>% filter(gene_id %in% c('ENSG00000104408','ENSG00000147655','ENSG00000144959','ENSG00000144962'))
-#exp_counts.s$gene <- c('EIF3E','NCEH1','SPATA16','RSPO2')
-#exp_counts.s.long <- exp_counts.s %>% gather(key='samp_id',value='values',-gene_id,-gene)
 
 p1 <- exp_counts_sub_data %>% 
   #dplyr::filter(!gene_name %in% c('PTPRK','RSPO3')) %>% 
   ggplot(aes(x = real_id, y = counts, fill=gene_loc)) + 
   geom_bar(stat = 'identity', position = 'dodge') +
-  #facet_wrap_paginate(~ pair + gene_name, scales = 'free', labeller = label_wrap_gen(multi_line = FALSE),
-   #                  nrow = 6, ncol = 2, page = 1)
   facet_wrap_paginate(~ pair + factor(gene_loc, levels = c('up_gene','dw_gene')),
                       scales = 'free', labeller = label_wrap_gen(multi_line = FALSE),
                                       ncol = 2, page = 4) +
-                     
-  #facet_wrap( ~ pair + factor(gene_loc, levels = c('up_gene','dw_gene')), 
-   #           ncol = 2, scales = 'free_y', labeller = label_wrap_gen(multi_line = F)) +
- # facet_grid(gene_name ~ pair,  scales = 'free_y') +
-  
-  theme(panel.background = element_blank(), axis.line = element_line(), 
-        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  #scale_x_discrete(labels = seq(1,37)) +
+                      theme(panel.background = element_blank(), axis.line = element_line(), 
+                      axis.text.x = element_text(angle = 90, vjust = 0.5)) +
   scale_fill_manual(values = c('steelblue3','red3'), name='')
-p1
 
 ggsave(p1, filename = 'fusion-pair-gene-exp-rnaseq-ab-fusions.eps', width = 10, height = 55)
 ggsave(p1, filename = 'fusion-pair-gene-exp-rnaseq.eps', device = cairo_ps, width = 10, height = 16, dpi = 300)
